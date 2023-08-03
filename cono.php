811eba1b4f6371a24daf64e5d8d2667e99ae18fc
Tejashwini2050 <107855898+Tejashwini2050@users.noreply.github.com>
2023-08-03 20:29:14 +0530

    Add files via upload

---
+++ cono.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Booking Confirmation</title>
+    <link rel="stylesheet" type="text/css" href="confirmation.css">
+</head>
+<body>
+    <header>
+        <h1>Booking Confirmation</h1>
+    </header>
+
+    <main>
+        <section>
+            <h2>Thank You for Your Booking!</h2>
+            <p>Your booking details:</p>
+            <div id="booking-details">
+                <?php
+                $host = 'localhost';
+                $dbUsername = 'root';
+                $dbPassword = '';
+                $dbName = 'hotel_hotel';
+               
+                $connection = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+            
+                if ($connection->connect_error) {
+                    die("Connection failed: " . $connection->connect_error);
+                }
+
+                // Get the 'cid' value from the URL
+                $cid = $_GET['cid'];
+                $bid = $_GET['bid'];
+               
+                // Prepare and execute the query using prepared statements
+                $sql = "SELECT cid, name, email, gender, phone_no FROM customer WHERE cid = ?";
+                $stmt = $connection->prepare($sql);
+                $stmt->bind_param("i", $cid);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "ID: " . $row['cid'] . "<br>";
+                        echo "Name: " . $row['name'] . "<br>";
+                        echo "Gender: " . $row['gender'] . "<br>";
+                        echo "Phone Number: " . $row['phone_no'] . "<br>";
+                        echo "Email: " . $row['email'] . "<br><br>";
+                    }
+                } else {
+                    echo "No data found.";
+                }
+
+                // Prepare and execute the query using prepared statements
+                $sql1 = "SELECT bid, check_in, check_out, room_type,no_of_guests FROM booking WHERE bid = ?";
+                $stmt = $connection->prepare($sql1);
+                $stmt->bind_param("i", $bid);
+                $stmt->execute();
+                $result1 = $stmt->get_result();
+                
+                if ($result1->num_rows > 0) {
+                    while ($row1 = $result1->fetch_assoc()) {
+                        echo "BID: " . $row1['bid'] . "<br>";
+                        echo "Check_IN: " . $row1['check_in'] . "<br>";
+                        echo "Check_OUT: " . $row1['check_out'] . "<br>";
+                        echo "ROOM_TYPE: " . $row1['room_type'] . "<br>";
+                        echo "Number Of Guests: " . $row1['no_of_guests'] . "<br>";
+                       
+                    }
+                } else {
+                    echo "No data found.";
+                }
+                
+                $connection->close();
+                ?>
+            </div>
+        </section>
+
+        <section>
+            <h2>Payment</h2>
+            <p>Click the button below to proceed to the payment page:</p>
+            <button id="payment-button">Proceed to Payment</button>
+        </section>
+    </main>
+</body>
+</html>
